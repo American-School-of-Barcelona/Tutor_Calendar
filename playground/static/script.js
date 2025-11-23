7d1a6f90a00e6de69f68af68d5f131e7a04a8fee
Krot101 <platon.malinin@gmail.com>
2025-11-23 18:07:33 +0100

    Added a DOM which will allow javascript to interact with HTML.
    Also added the js code for the calendar itself that creates a table header row with all the days, adds it to my table, and loops thru the dayOfWeek to create and append the header cells.

--- playground/static/script.js
+++ playground/static/script.js
@@ -1,3 +1,20 @@
+
 const today = new Date();
 const currentYear = today.getFullYear();
 const currentMonth = today.getMonth() + 1; // January is 0
+
+const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const timeSlots = [ '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '13:00 PM', '14:00 PM', '15:00 PM', '16:00 PM', '17:00 PM', '18:00 PM', '19:00 PM', '20:00 PM' ];
+
+doccument.addEventListener("DOMContentLoaded", function() {
+
+    const calendarTable = document.getElementById("calendar");
+
+    daysOfWeek.forEach(function(day) {
+        const headerCell = document.createElement("th");
+        headerCell.innerText = day;
+        headerRow.appendChild(headerCell);
+    });     
+
+    calendarTable.appendChild(headerRow);
+});
